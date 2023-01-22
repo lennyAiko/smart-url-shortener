b04d72c600b06e5c9aeb4a4404ee115638aff505
Lennox Charles <lennyaiko17@gmail.com>
2023-01-22 16:46:42 +0100

    done with model setup

--- shortener_app/database.py
+++ shortener_app/database.py
@@ -6,10 +6,12 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import get_settings
 
+# entry point to your db
 engine = create_engine(
     get_settings().db_url, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
 )
+# connects the database engine to the SQLAlchemy functionality of the models.
 Base = declarative_base()
