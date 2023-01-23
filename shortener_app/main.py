1186ab72e2b0d83d623215360ae37a8c484970f0
Lennox Charles <lennyaiko17@gmail.com>
2023-01-23 15:40:59 +0100

    redirect link now working

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -20,6 +20,10 @@ def get_db():
     finally:
         db.close()
 
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
@@ -47,3 +51,19 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
